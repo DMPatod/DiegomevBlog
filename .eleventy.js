dbc645a41df8d0f4bfdd1ce43d9fe4086294157c
DMPatod <danielmaiaevangelista@gmail.com>
2024-07-20 16:37:27 -0300

    chore: Add passthrough copy for CSS and JS files, update page collection sorting

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,13 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("./src/css/styles.css");
+  eleventyConfig.addPassthroughCopy("src/css/styles.css");
+  eleventyConfig.addPassthroughCopy("src/js/learning.js");
+
+  eleventyConfig.addCollection("pages", function (api) {
+    var pages = api.getFilteredByTag("pages").sort((a, b) => {
+      return b.page.date - a.page.date;
+    });
+    return pages.slice(pages.lenght - 2, 2);
+  });
 
   return {
     dir: {
