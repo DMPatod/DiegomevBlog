014fa8f245779fdda69eedd0b23008c17cc9c619
DMPatod <danielmaiaevangelista@gmail.com>
2024-09-23 14:56:45 -0300

    added sass lib

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,20 @@
+const sass = require("sass");
+
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addTemplateFormats("scss");
+
+  eleventyConfig.addExtension("scss", {
+    outputFileExtension: "css",
+
+    compile: async function (inputContent) {
+      let result = await sass.compileStringAsync(inputContent);
+
+      return async (data) => {
+        return result.css;
+      };
+    },
+  });
+
   eleventyConfig.addPassthroughCopy("src/css/styles.css");
   eleventyConfig.addPassthroughCopy("src/js/learning.js");
 
